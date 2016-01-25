cd92acd0808478e4dcb74c2315f0a3b6f54406cc
bbottema <b.bottema@projectnibble.org>
2016-01-25 14:17:21 +0000

    flow issues

--- index.js
+++ index.js
@@ -30,11 +30,13 @@ module.exports = function(settingsOrCondition, timeoutOrUndefined, intervalOrUnd
 
     var timedOut = false;
     var timeoutId = setTimeout(function() {
+      _log.log('[timeout occurred]');
       timedOut = true;
     }, settings.timeout);
 
     var waitingForResult = false;
     var intervalId = setInterval(function() {
+      _log.log('[interval triggered]', 'timedOut:', timedOut, 'waitingForResult:', waitingForResult, 'conditionResult:', conditionResult);
       if (!waitingForResult) {
         waitingForResult = true;
         settings.condition(function(conditionResult) {
@@ -57,6 +59,7 @@ module.exports = function(settingsOrCondition, timeoutOrUndefined, intervalOrUnd
     }, settings.interval);
 
     function finishWaitFor(success, errorMsg) {
+      _log.log('[finishWaitFor]');
       clearTimeout(timeoutId);
       clearInterval(intervalId);
       timeoutId = intervalId = null;
