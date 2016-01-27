ce25785871dc61c395822a2a7e2e2a54f913f652
bbottema <b.bottema@projectnibble.org>
2016-01-27 12:33:22 +0000

    cleanup

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
 var map = require('map-stream');
 var gutil = require('gulp-util');
 var waitForCondition = require('waitfor-condition');
-var _log = console.log;
 
 module.exports = function(settingsOrCondition, timeoutOrUndefined, intervalOrUndefined) {
   
   return map(function(file, cb) {
     waitForCondition(settingsOrCondition, timeoutOrUndefined, intervalOrUndefined).then(
       function()      { cb(null, file) },
-      function(error) { cb(error, file); }
+      function(error) { cb(new gutil.PluginError('gulp-waitfor', error.message), file); }
     );
   });
 };
